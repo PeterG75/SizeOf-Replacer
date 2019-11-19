9abe2167520b340212b3df8a0e18824b82193a25
Phemis <phemisz@gmail.com>
2019-11-19 20:18:42 +0300

    SizeOf Replacer
    SizeOf Replacer for .NET applications.

---
+++ SizeOf Replacer/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SizeOf_Replacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(106, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SizeOf Replacer\r\n      by\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(151, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RհօԵɑѵ";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(17, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 138);
+            this.panel1.TabIndex = 2;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel1_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(118, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Drag Here";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(17, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 37);
+            this.button1.TabIndex = 3;
+            this.button1.TabStop = false;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(212, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 37);
+            this.button2.TabIndex = 4;
+            this.button2.TabStop = false;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(345, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 21);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(6, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Not Loaded !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(382, 273);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SizeOf Replacer ~ RհօԵɑѵ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SizeOf Replacer/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using dnlib.DotNet.Emit;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace SizeOf_Replacer
+{
+    public partial class Form1 : Form
+    {
+        string directoryName = "";
+        string filePath = "";
+        static ModuleDefMD module = null;
+        public Thread thr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Rhotav");
+        }
+
+        private void Panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".");
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            int num2 = text.LastIndexOf("\\");
+                            if (num2 != -1)
+                            {
+                                directoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (directoryName.Length == 2)
+                            {
+                                directoryName += "\\";
+                            }
+                            module = ModuleDefMD.Load(text);
+                            filePath = text;
+                            pictureBox1.BackColor = Color.Lime;
+                            label4.Text = "Loaded !";
+                            label4.ForeColor = Color.Lime;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                pictureBox1.BackColor = Color.Red;
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Lime;
+            }
+        }
+
+        private void Panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if (filePath == string.Empty) return;
+
+            MessageBox.Show(filePath,"SizeOf Replacer",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Executable Files|*.exe|DLL Files |*.dll";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    module = ModuleDefMD.Load(open.FileName);
+                    filePath = open.FileName;
+                    pictureBox1.BackColor = Color.Lime;
+                    label4.Text = "Loaded !";
+                    label4.ForeColor = Color.Lime;
+                }
+            }
+            catch(Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message , "Error !" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+                pictureBox1.BackColor = Color.Red;
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Lime;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(filePath != string.Empty && module != null)
+            {
+                thr = new Thread(new ThreadStart(CodeBlock));
+                thr.Start();
+            }
+        }
+        public void CodeBlock()
+        {
+            try
+            {
+                int decrypted = 0;
+
+                foreach (TypeDef type in module.Types)
+                {
+                    if (!type.HasMethods) continue;
+                    foreach (MethodDef method in type.Methods)
+                    {
+                        if (!method.HasBody) continue;
+
+                        for (int i = 0; i < method.Body.Instructions.Count; i++)
+                        {
+                            if (method.Body.Instructions[i].OpCode == OpCodes.Sizeof)
+                            {
+                                int value = ReturnSizeOf(method.Body.Instructions[i].Operand.ToString());
+                                method.Body.Instructions[i].OpCode = OpCodes.Ldc_I4;
+                                method.Body.Instructions[i].Operand = value;
+
+                                i += 1;
+                                decrypted++;
+                            }
+                        }
+
+                    }
+                }
+                SaveAssembly();
+                MessageBox.Show("Replaced " + decrypted.ToString() + " sizeOf !", "SizeOf Replacer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        static void SaveAssembly()
+        {
+            var writerOptions = new NativeModuleWriterOptions(module, true);
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            writerOptions.MetadataOptions.Flags = (MetadataFlags.PreserveTypeRefRids | MetadataFlags.PreserveTypeDefRids | MetadataFlags.PreserveFieldRids | MetadataFlags.PreserveMethodRids | MetadataFlags.PreserveParamRids | MetadataFlags.PreserveMemberRefRids | MetadataFlags.PreserveStandAloneSigRids | MetadataFlags.PreserveEventRids | MetadataFlags.PreservePropertyRids | MetadataFlags.PreserveTypeSpecRids | MetadataFlags.PreserveMethodSpecRids | MetadataFlags.PreserveStringsOffsets | MetadataFlags.PreserveUSOffsets | MetadataFlags.PreserveBlobOffsets | MetadataFlags.PreserveAll | MetadataFlags.AlwaysCreateGuidHeap | MetadataFlags.PreserveExtraSignatureData | MetadataFlags.KeepOldMaxStack);
+            module.NativeWrite(Path.GetDirectoryName(module.Location) + @"\" + Path.GetFileNameWithoutExtension(module.Location) + "_sizeOf.exe", writerOptions);
+        }
+        public static int ReturnSizeOf(string deger)
+        {
+            switch (deger)
+            {
+                case "System.SByte":
+                    return 1;
+                case "System.Boolean":
+                    return 1;
+                case "System.Decimal":
+                    return 16;
+                case "System.Int16":
+                    return 2;
+                case "System.Int32":
+                    return 4;
+                case "System.Byte":
+                    return 1;
+                case "System.Int64":
+                    return 8;
+                case "System.Single":
+                    return 4;
+            }
+            return 0;
+        }
+    }
+}
